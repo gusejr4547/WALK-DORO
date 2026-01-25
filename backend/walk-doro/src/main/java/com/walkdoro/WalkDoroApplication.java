9d89fa624329f3845f6fa02249eb277b8c0348d5
조현덕 <gusejr4547@gmail.com>
2026-01-26 00:30:15 +0900

    feat: Initialize Spring Boot project

---
+++ backend/walk-doro/src/main/java/com/walkdoro/WalkDoroApplication.java
@@ -0,0 +1,13 @@
+package com.walkdoro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalkDoroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalkDoroApplication.class, args);
+	}
+
+}
---
+++ backend/walk-doro/src/test/java/com/walkdoro/WalkDoroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walkdoro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalkDoroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
